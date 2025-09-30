2ddfa69ec86c55b73106b34214841b1a82d4b85a
Baptou302 <kikijeamichelle@gmail.com>
2025-09-30 16:40:12 +0200

    avancée

--- main.go
+++ main.go
@@ -1,16 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"strconv"
+
+	"power4/internal/game"
 )
 
+var tpl = template.Must(template.ParseFiles("internal/templates/index.html"))
+
 func main() {
+	g := game.New()
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "test")
-		fmt.Fprintln(w, "test")
+		data := map[string]interface{}{
+			"Board":         g.Board,
+			"CurrentPlayer": g.CurrentPlayer,
+			"Winner":        g.Winner,
+			"Finished":      g.Finished,
+			"Columns":       []int{0, 1, 2, 3, 4, 5, 6},
+		}
+		if err := tpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
-	fmt.Println("Serveur démarré sur le port 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		colStr := r.FormValue("col")
+		col, err := strconv.Atoi(colStr)
+		if err == nil {
+			_ = g.Play(col) // on ignore l'erreur côté front (option: passer message d'erreur)
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			g.Reset()
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
+	log.Println("Listening on :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
